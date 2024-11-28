00bca6eab970261deeeae0610eee4723b1537307
Sarvesh Bajaj <bt22cse087@iiitn.ac.in>
2024-11-28 23:15:03 +0530

    some updates2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,18 +3,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { config } from "dotenv";
 
-// Load environment variables from .env file
 config();
-const __dirname = path.resolve();
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // Alias for easy imports
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: "dist", // Ensure the build output is in the dist folder
-  }
+  define: {
+    'process.env': import.meta,
+  },
 });
